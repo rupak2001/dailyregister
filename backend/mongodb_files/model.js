46c5c037652e5d6b8926441a95e4c723cf97894c
RUPAK <rupak@localhost.localdomain>
2021-04-18 20:34:18 +0530

    made email verification sytem up and running :]

---
+++ backend/mongodb_files/model.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose')
+const validator = require('validator')
+
+const sc = mongoose.Schema({
+    "name":{type:String},
+    "email":{type:String,
+    unique:[true,'this email already exists'],
+    validate(val){
+        if(!validator.isEmail){
+            throw new error('not a valid email');
+        }
+    }
+    },
+    "phone_no":{type:Number,maxlength:12,minlength:10,unique:[true,'this no. already exists']},
+    "password":{type:String,unique:[true,"password already taken"]},
+    "is_signedup" : {type:Boolean,default:false},
+    "unique_code":String
+})
+
+const reg_info = new mongoose.model("req_info",sc);
+
+module.exports = reg_info;
--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,77 @@
 const express = require("express");
+const nodemailer = require("nodemailer")
 const app = express();
 const port = process.env.PORT || 8000;
+const model = require('./mongodb_files/model');
+require('./mongodb_files/db');
+
+app.use(express.json());
+
+//create random numbers
+const rand_num = ()=>{
+    var authval = '';
+
+    for(var i = 0;i<8;i++){
+        authval += Math.floor((Math.random()*10)+1);
+    }
+    return authval
+}
 
 
-app.get("/",(req,res)=>{
-    res.send("hello world");
+//total mail mechanism
+const send_email = (email,authkey)=>{
+    var Transport = nodemailer.createTransport({
+        service: "Gmail",
+        auth:{
+            user:"rupsrb2001@gmail.com",
+            pass:"rups1957"
+        }
+    });
+
+    
+
+    var mailOptions = {
+        from: "noreply@checkmail.com",
+        to: email,
+        subject: "confirmation email",
+        html: 'click <a href= http://localhost:8000/verify/'+email+'/'+authkey+'>here</a> to verify'
+    }
+
+    Transport.sendMail(mailOptions,(err,res)=>{
+        if(err){
+            console.log(err);
+        }
+        else{
+            console.log("message sent");
+        }
+    })
+}
+
+var authkey = '';
+
+app.post('/login',async(req,res)=>{
+    ini_mod = new model(req.body);
+    authkey = rand_num();
+    console.log(authkey);
+    send_email(req.body.email,authkey);
+    var run = await model.insertMany([ini_mod]);
+    console.log(run);
+    res.send(run);
+})
+
+
+
+app.get("/verify/:email/:code",async(req,res)=>{
+    if(req.params.code == authkey){
+        var run = await model.updateOne({email:req.params.email},{is_signedup:true})
+        res.redirect('/') //needed to change acc to frontend
+    }
+    else{
+        res.send('user not found');
+    }
 });
 
 
-app.listen(port);
+app.listen(port,()=>{
+    console.log('server connected at port',port);
+});
